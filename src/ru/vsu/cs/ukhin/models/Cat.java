3c719081cd32a9eee11649ba592b1c4a56738921
MaxOmenes <ukhin.maxim@gmail.com>
2023-09-22 00:37:17 +0300

    Better Face

--- src/ru/vsu/cs/ukhin/models/Cat.java
+++ src/ru/vsu/cs/ukhin/models/Cat.java
@@ -6,10 +6,14 @@ import java.awt.*;
 
 public class Cat {
     private Color color;
-    private static final Color strokeColor = Color.black;
+
     public Cat(Color color){
         this.color = color;
     }
+
+    //color
+    private static final Color strokeColor = Color.black;
+    private static final Color earsColor = Color.decode("#fac0cd");
 
     //size
     private static final int HEIHGT = 500;
@@ -17,6 +21,8 @@ public class Cat {
 
     // indent
     private static final int headIndent = 25;
+    private static final int eyesIndent = 50;
+
 
     //body size
     private static final int headWidth = WIDTH - 2 * headIndent;
@@ -26,21 +32,36 @@ public class Cat {
     private static final int earsHeight = headWidth/2;
     private static final int earsRotate = 60;
 
+    private void drawEar(Graphics2D g2d, int x, int y, int width, int height, int rotate){
+        g2d.setStroke(new BasicStroke(20)); //right ear
+        g2d.setColor(strokeColor);
+        g2d.rotate(rotate, x, y);
+        g2d.drawOval(x, y, width, height);
+        g2d.setStroke(new BasicStroke(10));
+        g2d.setColor(color);
+        g2d.drawOval(x, y, width, height);
+        g2d.setColor(earsColor);
+        g2d.fillOval(x, y, width, height);
 
-    public void draw(Graphics2D g2d, int x, int y){
-        g2d.setStroke(new BasicStroke(5));
+        g2d.rotate(-rotate, x, y); //return to 0
+    }
+
+    public void draw(Graphics2D g2d, int x, int y){ //x, y - centre
+        //draw ears
+        drawEar(g2d, x, y, earsWidth, earsHeight,earsRotate+45); //right
+        drawEar(g2d, x, y, earsHeight, earsWidth, earsRotate); //left
+
+        g2d.setStroke(new BasicStroke(10));
         //draw head
         g2d.setColor(strokeColor);
-        g2d.drawOval(x+headIndent, y+headIndent, headWidth, headHeight);
+        g2d.drawOval(x-headWidth/2, y-headHeight/2, headWidth, headHeight);
+        g2d.setColor(color);
+        g2d.fillOval(x-headWidth/2, y-headHeight/2, headWidth, headHeight);
 
-        //draw ears
-        g2d.setColor(strokeColor);
-        g2d.rotate(Math.toDegrees(earsRotate));
-        g2d.drawOval(x+headIndent+headWidth/2, y-headHeight-headIndent, earsWidth, earsHeight);
-
-        g2d.setColor(strokeColor);
-        g2d.rotate(Math.toDegrees(180-earsRotate));
-        g2d.drawOval(x, y-headIndent-headHeight, earsWidth, earsHeight);
+        //draw eyes
+        g2d.setColor(Color.BLACK);
+        g2d.fillOval(x+eyesIndent, y-eyesIndent, 40, 50);
+        g2d.fillOval(x-eyesIndent, y-eyesIndent, 40, 50);
 
     }
 }
